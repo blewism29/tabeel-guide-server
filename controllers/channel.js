1f9b774a6463997bc75edfa9cfe45330a49c12e2
Benjamin Lewis <blewis@lacreativeria.com>
2018-09-16 03:14:43 -0600

    master | Add controllers instead of doing logic in router

--- controllers/channel.js
+++ controllers/channel.js
@@ -2,32 +2,30 @@
  * @Author: Benjamin Lewis 
  * @Date: 2018-09-16 01:33:19 
  * @Last Modified by: Benjamin Lewis
- * @Last Modified time: 2018-09-16 03:12:46
+ * @Last Modified time: 2018-09-16 03:11:53
  */
 
 /* ===================================== */
 /*               IMPORTS                 */
 /* ===================================== */
 
-const controller = require('../controllers/channel');
+const ChannelInteractor = require('../interactors/channel');
+const presenter = require('../presenters/channel');
 
 /* ===================================== */
 /*             DECLARATIONS              */
 /* ===================================== */
 
-const baseUrl = '/channels'
+const interactor = new ChannelInteractor(null);
 
 /* ===================================== */
 /*                 CLASS                 */
 /* ===================================== */
 
 module.exports = {
-  addRoutes: (app) => {
-    if (!app) throw new Error('App parameter is required');
-    
-    /* 
-      Get all channels stored in database.
-    */
-    app.get(baseUrl, controller.getChannels);
-  } 
+  getChannels: (req, res) => {
+    const channels = interactor.getChannels(null);
+    const response = presenter.decorateChannels(channels);
+    res.send(response);
+  }
 }
